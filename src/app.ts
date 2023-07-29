7e604b815e1a1b781afb43a583d9337e8bd0241f
Nguyen Quang Tri <nguyenquangtri@Nguyens-Mac-mini.local>
2023-07-29 12:11:10 +0700

    Add rate limiting

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,33 @@
-import express, { Express, Request, Response } from "express";
-import { ErrorHandler } from "./http/middlewares/ErrorHandler";
+import express, { Express, Request, Response } from "express"
+import { ErrorHandler } from "./http/middlewares/ErrorHandler"
 
-import bodyParser from "body-parser";
-import cors from "cors";
-import itineraryRoute from "./routes/itinerary";
+import bodyParser from "body-parser"
+import cors from "cors"
+import itineraryRoute from "./routes/itinerary"
+import rateLimit from "express-rate-limit"
 
-const app: Express = express();
+const app: Express = express()
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Apply rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+})
+app.use(limiter)
 
-app.use("/itinerary", itineraryRoute);
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use("/itinerary", itineraryRoute)
 
 app.use("*", (req: Request, res: Response) => {
   return res.status(404).json({
     success: false,
     message: "Invalid route",
-  });
-});
+  })
+})
 
-app.use(ErrorHandler.handleErrors);
+app.use(ErrorHandler.handleErrors)
 
-export default app;
+export default app
